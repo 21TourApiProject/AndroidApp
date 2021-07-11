8da0080e4d0051dec2c177d91b519e1e1a84e0aa
parkjinhyeok348 <wlsgur348@gmail.com>
2021-07-11 22:19:02 +0900

    second commit

--- app/src/main/java/com/starrynight/tourapiproject/MainActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.starrynight.tourapiproject;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -49,14 +50,14 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        Button buttonOpen = (Button) findViewById(R.id.open) ;
-        buttonOpen.setOnClickListener(new Button.OnClickListener() {
+        Button button =findViewById(R.id.weather_button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer) ;
-                if (!drawer.isDrawerOpen(Gravity.START)) {
-                    drawer.openDrawer(Gravity.START) ;
-                }
+                Intent intent = new Intent(getApplicationContext(),weatherActivity.class);
+                startActivity(intent);
+
+                finish();
             }
         });
 
