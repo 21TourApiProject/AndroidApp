677d1bd3a951625edef0681355578c460ca72f57
sein0211 <sein4037@gmail.com>
2021-07-13 01:07:39 +0900

    게시물 작성

--- app/src/main/java/com/starrynight/tourapiproject/postWritePage/SearchObservingPointActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/postWritePage/SearchObservingPointActivity.java
@@ -2,14 +2,30 @@ package com.starrynight.tourapiproject.postWritePage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.starrynight.tourapiproject.R;
 
 public class SearchObservingPointActivity extends AppCompatActivity {
+    TextView findObservePoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_observing_point);
+
+        findObservePoint = findViewById(R.id.findObservePoint);
+        Button addObservePoint = findViewById(R.id.addObservePoint);
+        addObservePoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (findObservePoint != null){
+
+                }
+            }
+        });
     }
+
 }
