cce8be0003399cfacbb6c5311dfc2d4d83364a24
dingding-21 <phj8221@naver.com>
2021-07-13 20:47:21 +0900

    overall layout

---
+++ app/src/main/java/com/starrynight/tourapiproject/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.starrynight.tourapiproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.starrynight.tourapiproject.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
