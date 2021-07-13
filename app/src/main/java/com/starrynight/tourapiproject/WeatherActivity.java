f2abe7ab99590200708739b580152d21cc12d4aa
dingding-21 <phj8221@naver.com>
2021-07-13 21:33:14 +0900

    Resolving errors with main

--- app/src/main/java/com/starrynight/tourapiproject/WeatherActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/WeatherActivity.java
@@ -5,14 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class weatherActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        Button button =findViewById(R.id.back_button);
+        ImageButton button =findViewById(R.id.wt_back_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
