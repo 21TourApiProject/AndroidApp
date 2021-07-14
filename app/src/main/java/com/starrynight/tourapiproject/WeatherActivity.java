0952b8ee57e95351e3ea6e5f19eb872d7ad73d25
dingding-21 <phj8221@naver.com>
2021-07-14 21:48:19 +0900

    오늘의 구름지도 링크 연결

--- app/src/main/java/com/starrynight/tourapiproject/WeatherActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.starrynight.tourapiproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,5 +23,16 @@ public class WeatherActivity extends AppCompatActivity {
             }
         });
 
+        // 기상청 구름 정보 페이지로 이동
+        Button button1 = findViewById(R.id.wt_today_cloud);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.weather.go.kr/w/index.do"));
+                startActivity(intent);
+            }
+        });
     }
+    
 }
