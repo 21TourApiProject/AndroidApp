9f97e4e07e27a0f67834d9d2bdab39d88bb5d3fe
gyulchyoung <hacha2011@naver.com>
2021-07-15 15:52:02 +0900

    add diffrent custom marker and make BalloonObject

--- app/src/main/java/com/starrynight/tourapiproject/map/MapFragment.java
+++ app/src/main/java/com/starrynight/tourapiproject/map/MapFragment.java
@@ -5,8 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.starrynight.tourapiproject.R;
@@ -18,9 +16,10 @@ import net.daum.mf.map.api.MapView;
 
 
 public class MapFragment extends Fragment {
-
-    private MapPOIItem mCustomMarker;
-    private static final MapPoint CUSTOM_MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);
+    //마커, 위치, 오브젝트 생성
+    private MapPOIItem mMarker;
+    private MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);
+    BalloonObject balloon_Object = new BalloonObject();
 
     public MapFragment() {
         // Required empty public constructor
@@ -42,8 +41,10 @@ public class MapFragment extends Fragment {
 
         @Override
         public View getCalloutBalloon(MapPOIItem poiItem) {
+            //말풍선 내용 설정
+            BalloonObject bobject = (BalloonObject)poiItem.getUserObject();
             ((TextView) mCalloutBalloon.findViewById(R.id.title)).setText(poiItem.getItemName());
-            ((TextView) mCalloutBalloon.findViewById(R.id.desc)).setText("Custom CalloutBalloon");
+            ((TextView) mCalloutBalloon.findViewById(R.id.desc)).setText(bobject.getContent());
             return mCalloutBalloon;
         }
 
@@ -62,7 +63,6 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
         MapView mapView = new MapView(getActivity());
@@ -70,46 +70,70 @@ public class MapFragment extends Fragment {
         ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
 
-        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304), true);
+        //지도 중심점 설정
+       mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304), true);
         mapView.setCalloutBalloonAdapter(new CustomCalloutBalloonAdapter());
 
         // 줌 레벨 변경
         mapView.setZoomLevel(4, true);
 
-        createCustomMarker(mapView);
+        setupMaker(1,"1100고지", "제주도에 있는 관측지", 37.54892296550104, 126.99089033876304);
+        if(balloon_Object.getTag()==1)
+            createObserveMarker(mapView);
+        else if(balloon_Object.getTag()==2)
+            createTourMarker(mapView);
 
-        //마커 찍기
-//        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);
-//        MapPOIItem marker = new MapPOIItem();
-//        marker.setItemName("Default Marker");
-//        marker.setTag(0);
-//        marker.setMapPoint(MARKER_POINT);
-//        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
-//        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
-//
-//        mapView.addPOIItem(marker);
-
-
+        setupMaker(2,"안반데기", "강릉에 있는 관측지", 37.53737528, 127.00557633);
+        if(balloon_Object.getTag()==1)
+            createObserveMarker(mapView);
+        else if(balloon_Object.getTag()==2)
+            createTourMarker(mapView);
 
         return view;
     }
 
-    private void createCustomMarker(MapView mapView) {
-        mCustomMarker = new MapPOIItem();
-        String name = "Custom Marker";
-        mCustomMarker.setItemName(name);
-        mCustomMarker.setTag(1);
-        mCustomMarker.setMapPoint(CUSTOM_MARKER_POINT);
+    private void createObserveMarker(MapView mapView) {
+        //관측지 마커 생성, 태그는 1번, 파란핀
+        mMarker = new MapPOIItem();
+        mMarker.setItemName(balloon_Object.getName());
+        mMarker.setTag(1);
+        mMarker.setMapPoint(MARKER_POINT);
+        mMarker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 마커타입을 지정
+        mMarker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        mMarker.setUserObject(balloon_Object);   //
 
-        mCustomMarker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 마커타입을 지정
-        mCustomMarker.setSelectedMarkerType(MapPOIItem.MarkerType.YellowPin);
 //        mCustomMarker.setCustomImageResourceId(R.drawable.custom_marker_red); //마커타입을 커스텀으로 지정 후 이용
 //        mCustomMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
 //        mCustomMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(mMarker);
+        mapView.selectPOIItem(mMarker, true);
+        mapView.setMapCenterPoint(MARKER_POINT, false);
+    }
 
-        mapView.addPOIItem(mCustomMarker);
-        mapView.selectPOIItem(mCustomMarker, true);
-        mapView.setMapCenterPoint(CUSTOM_MARKER_POINT, false);
+    private void createTourMarker(MapView mapView) {
+        //관광지 마커 생성, 태그는 2번, 노란핀
+        mMarker = new MapPOIItem();
+        mMarker.setItemName(balloon_Object.getName());
+        mMarker.setTag(2);
+        mMarker.setMapPoint(MARKER_POINT);
+        mMarker.setMarkerType(MapPOIItem.MarkerType.YellowPin); // 마커타입을 지정
+        mMarker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        mMarker.setUserObject(balloon_Object);   //
 
+//        mCustomMarker.setCustomImageResourceId(R.drawable.custom_marker_red); //마커타입을 커스텀으로 지정 후 이용
+//        mCustomMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+//        mCustomMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(mMarker);
+        mapView.selectPOIItem(mMarker, true);
+        mapView.setMapCenterPoint(MARKER_POINT, false);
+    }
+
+    private void setupMaker(int tag, String name, String content, double  latitude, double longitude)
+    {
+        balloon_Object.setName(name);
+        balloon_Object.setContent(content);
+        balloon_Object.setTag(tag);
+
+        MARKER_POINT=MapPoint.mapPointWithGeoCoord(latitude, longitude);
     }
 }
