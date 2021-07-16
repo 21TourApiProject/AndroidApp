6f986985251d472f737a1568c5708d2df821fc4f
dingding-21 <phj8221@naver.com>
2021-07-16 15:36:55 +0900

    custom DatePicker 2차 commit (월,일)

--- app/src/main/java/com/starrynight/tourapiproject/WeatherActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/WeatherActivity.java
@@ -5,20 +5,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class WeatherActivity extends AppCompatActivity {
 
-    Button wtDatePicker;
+    private TextView wtDatePicker;
 
     DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-            Log.d("YearMonthPickerTest", "year = " + year + ", month = " + month + ", day = " + dayOfMonth);
+            wtDatePicker.setText(month + "월 " + dayOfMonth + "일");
         }
     };
 
@@ -31,19 +31,10 @@ public class WeatherActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
-
             }
         });
 
         wtDatePicker = findViewById(R.id.wt_datePicker);
-        wtDatePicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                WtDatePickerDialog pd = new WtDatePickerDialog();
-                pd.setListener(d);
-                pd.show(getFragmentManager(), "test");
-            }
-        });
 
         // 기상청 구름 정보 페이지로 이동
         Button button1 = findViewById(R.id.wt_today_cloud);
@@ -55,6 +46,15 @@ public class WeatherActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void wtClickDatePicker(View view) {
+        WtDatePickerDialog pd = new WtDatePickerDialog();
+        pd.setListener(d);
+        pd.show(getFragmentManager(), "test");
+    }
+
+    public void wtClickAreaPicker(View view) {
 
     }
 
