cdd6c00e8ff2ac4f36a52cfa21cceebba69e64f7
parkjinhyeok348 <wlsgur348@gmail.com>
2021-07-18 16:10:31 +0900

    마이그레이션 commit

--- app/src/main/java/com/starrynight/tourapiproject/MainActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/MainActivity.java
@@ -1,31 +1,15 @@
 package com.starrynight.tourapiproject;
 
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
-import android.util.Log;
-import android.view.Gravity;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 //세인 주석
 public class MainActivity extends AppCompatActivity {
 //주석 추가
