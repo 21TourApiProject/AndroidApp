91ca4e4799301c770ededfa9a27a987c62e81ed2
sein0211 <sein4037@gmail.com>
2021-07-22 18:00:55 +0900

    해시태그 추가

--- app/src/main/java/com/starrynight/tourapiproject/signUpPage/SignUpActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/signUpPage/SignUpActivity.java
@@ -2,9 +2,13 @@ package com.starrynight.tourapiproject.signUpPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.starrynight.tourapiproject.R;
+import com.starrynight.tourapiproject.weatherPage.WeatherActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button test = findViewById(R.id.signUp);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, SelectHashTagActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
