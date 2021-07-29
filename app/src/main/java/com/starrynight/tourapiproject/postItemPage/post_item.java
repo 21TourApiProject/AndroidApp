a523de9ad9cd131ed317e50224c753582a881b01
parkjinhyeok348 <wlsgur348@gmail.com>
2021-07-29 15:47:26 +0900

    메인, 별자리,관측지,관광지 레이아웃 및 화면 이동 commit

--- app/src/main/java/com/starrynight/tourapiproject/postItemPage/post_item.java
+++ app/src/main/java/com/starrynight/tourapiproject/postItemPage/post_item.java
@@ -1,12 +1,64 @@
 package com.starrynight.tourapiproject.postItemPage;
 
+import android.media.Image;
+
 public class post_item {
     String hash;
     String hash2;
+    String title;
+    String nickname;
+    String review;
+    String image;
+    String image2;
 
-    public post_item(String hash, String hash2) {
+    public post_item(String hash, String hash2, String title, String nickname, String review,String image, String image2) {
         this.hash = hash;
         this.hash2 = hash2;
+        this.title = title;
+        this.nickname = nickname;
+        this.review = review;
+        this.image = image;
+        this.image2= image2;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImage2() {
+        return image2;
+    }
+
+    public void setImage2(String image2) {
+        this.image2 = image2;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
     }
 
     public String getHash() {
--- app/src/main/java/com/starrynight/tourapiproject/postItemPage/post_point_item.java
+++ app/src/main/java/com/starrynight/tourapiproject/postItemPage/post_point_item.java
@@ -2,10 +2,21 @@ package com.starrynight.tourapiproject.postItemPage;
 
 public class post_point_item {
     String tourname;
+    String tourimage;
 
-    public post_point_item(String tourname) {
+    public post_point_item(String tourname,String tourimage) {
         this.tourname = tourname;
+        this.tourimage= tourimage;
     }
+
+    public String getTourimage() {
+        return tourimage;
+    }
+
+    public void setTourimage(String tourimage) {
+        this.tourimage = tourimage;
+    }
+
     public String getTourname(){return tourname; }
     public void setTourname(String tourname) {
         this.tourname = tourname;
