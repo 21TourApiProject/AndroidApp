ccc5aae134ca04772862b461da320f6adbc7b701
dingding-21 <phj8221@naver.com>
2021-08-02 13:54:01 +0900

    지역 선택 spinner 연결

--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/WeatherActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/WeatherActivity.java
@@ -7,9 +7,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -41,6 +44,10 @@ public class WeatherActivity extends AppCompatActivity {
     private TimePickerDialog.OnTimeSetListener timeListener;
 
     String API_KEY;
+
+    ArrayAdapter<CharSequence> cityAdSpin, provAdSpin;
+    String choice_do = "";
+    String choice_se = "";
 
     {
         try {
@@ -154,7 +161,241 @@ public class WeatherActivity extends AppCompatActivity {
         @Override
         public void onFailure(Call<WtMetModel> call, Throwable t) {
             t.printStackTrace();
-            Log.v("My Tag", "response= " + call.request().url());
+            Log.v("My Tag", "responseError= " + call.request().url());
         }
     };
+
+    //출몰시각 API 연결
+    private Callback<WtAppearTimeModel> appearTimeModelCallback = new Callback<WtAppearTimeModel>() {
+        @Override
+        public void onResponse(Call<WtAppearTimeModel> call, Response<WtAppearTimeModel> response) {
+            if (response.isSuccessful()) {
+                WtAppearTimeModel data = response.body();
+                TextView textView1 = findViewById(R.id.wt_cloud_info);
+                textView1.setText(data.getResponse().getBody().getTotalCount());
+                Log.d("AppearTime", "response= " + response.raw().request().url().url());
+            }
+
+        }
+
+        @Override
+        public void onFailure(Call<WtAppearTimeModel> call, Throwable t) {
+            t.printStackTrace();
+            Log.v("AppearTime", "responseError= " + call.request().url());
+        }
+    };
+
+    //시,도 Spinner 동작
+    public void onSetAreaSpinner() {
+        final Spinner citySpinner = (Spinner) findViewById(R.id.wt_citySpinner);
+        final Spinner provSpinner = (Spinner) findViewById(R.id.wt_provinceSpinner);
+
+        cityAdSpin = ArrayAdapter.createFromResource(this, R.array.wt_cityList, android.R.layout.simple_spinner_dropdown_item);
+        cityAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        citySpinner.setAdapter(cityAdSpin);
+        citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (cityAdSpin.getItem(position).equals("서울")) {
+                    choice_do = "서울";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_Seoul, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("경기")) {
+                    choice_do = "경기";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_Gyeonggi, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("인천")) {
+                    choice_do = "인천";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_Incheon, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("강원")) {
+                    choice_do = "강원";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_Gangwon, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("충북")) {
+                    choice_do = "충북";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_Chungbuk, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("충남/대전/세종")) {
+                    choice_do = "충남/대전/세종";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_ChungnamDaejeonSejong, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("광주/전북")) {
+                    choice_do = "광주/전북";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_GwangjuJeonbuk, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("전남")) {
+                    choice_do = "전남";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_Jeonnam, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("대구/경북")) {
+                    choice_do = "대구/경북";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_DaeguGyeongbuk, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("경남")) {
+                    choice_do = "경남";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_Gyeongnam, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("부산/울산")) {
+                    choice_do = "부산/울산";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_BusanUlsan, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                } else if (cityAdSpin.getItem(position).equals("제주")) {
+                    choice_do = "제주";
+                    provAdSpin = ArrayAdapter.createFromResource(WeatherActivity.this, R.array.wt_Jeju, android.R.layout.simple_spinner_dropdown_item);
+                    provAdSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    provSpinner.setAdapter(provAdSpin);
+                    provSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = provAdSpin.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
 }
