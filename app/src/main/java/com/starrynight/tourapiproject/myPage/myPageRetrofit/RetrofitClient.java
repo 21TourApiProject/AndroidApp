d46e5151575a648e5340bf593468aa8c29712b70
sein0211 <sein4037@gmail.com>
2021-08-03 23:20:09 +0900

    마이페이지

--- app/src/main/java/com/starrynight/tourapiproject/myPage/myPageRetrofit/RetrofitClient.java
+++ app/src/main/java/com/starrynight/tourapiproject/myPage/myPageRetrofit/RetrofitClient.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.23.80.190:8080/v1/";
+    private static final String BASE_URL = "http://172.30.1.6:8080/v1/";
 
     public static MyPageRetrofitService getApiService(){return getInstance().create(MyPageRetrofitService.class);}
 
