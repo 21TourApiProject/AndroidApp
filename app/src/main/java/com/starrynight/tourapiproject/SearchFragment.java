50e5bd267211e9042263502d55a4c1f9279fe14a
sein0211 <sein4037@gmail.com>
2021-08-03 23:28:22 +0900

    map -> mapPage

--- app/src/main/java/com/starrynight/tourapiproject/SearchFragment.java
+++ app/src/main/java/com/starrynight/tourapiproject/SearchFragment.java
@@ -9,7 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.starrynight.tourapiproject.touristSpotPage.Touristspot_Activity;
-import com.starrynight.tourapiproject.map.MapFragment;
+import com.starrynight.tourapiproject.mapPage.MapFragment;
 
 
 /**
