e2cd8fbd2e0bbb87d79d5a3a3d61bfb0b252f724
dingding-21 <phj8221@naver.com>
2021-08-04 13:54:06 +0900

    BottomNavigationView 사라지게 하기 및 뒤로가기 버튼 추가

--- app/src/main/java/com/starrynight/tourapiproject/starPage/TonightSkyFragment.java
+++ app/src/main/java/com/starrynight/tourapiproject/starPage/TonightSkyFragment.java
@@ -1,13 +1,22 @@
 package com.starrynight.tourapiproject.starPage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ortiz.touchview.TouchImageView;
+import com.starrynight.tourapiproject.MainActivity;
 import com.starrynight.tourapiproject.R;
+import com.starrynight.tourapiproject.postWritePage.PostWriteActivity;
 
 public class TonightSkyFragment extends Fragment {
 
@@ -23,7 +32,15 @@ public class TonightSkyFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_tonight_sky, container, false);
 
 
+        ImageButton backBtn =(ImageButton) v.findViewById(R.id.star_back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return v;
     }
-
 }
