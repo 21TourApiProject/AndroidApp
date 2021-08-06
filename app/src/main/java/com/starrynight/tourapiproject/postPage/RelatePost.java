1771ec3886e5c8e3e992cc9f5ff24a9ca7091c60
parkjinhyeok348 <wlsgur348@gmail.com>
2021-08-06 22:19:22 +0900

    post server commit

--- app/src/main/java/com/starrynight/tourapiproject/postPage/RelatePost.java
+++ app/src/main/java/com/starrynight/tourapiproject/postPage/RelatePost.java
@@ -3,19 +3,19 @@ package com.starrynight.tourapiproject.postPage;
 import android.graphics.Bitmap;
 
 public class RelatePost {
-    int img; //지금은 테스트를 위해 drawble에 있는 이미지를 넣으려고 int를 썼지만, db data를 이용하려면 Bitmap로 바꿔야할듯
+    Bitmap img; //지금은 테스트를 위해 drawble에 있는 이미지를 넣으려고 int를 썼지만, db data를 이용하려면 Bitmap로 바꿔야할듯
     String title;
 
-    public RelatePost(int img, String title) {
+    public RelatePost(Bitmap img, String title) {
         this.img = img;
         this.title = title;
     }
 
-    public int getImg() {
+    public Bitmap getImg() {
         return img;
     }
 
-    public void setImg(int img) {
+    public void setImg(Bitmap img) {
         this.img = img;
     }
 
---
+++ app/src/main/java/com/starrynight/tourapiproject/postPage/postRetrofit/RetrofitClient.java
@@ -0,0 +1,22 @@
+package com.starrynight.tourapiproject.postPage.postRetrofit;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.starrynight.tourapiproject.postWritePage.postWriteRetrofit.PostHashTagRetrofitService;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static final String BASE_URL = "http://172.30.1.24:8080/v1/";
+
+    public static PostPageRetrofitService getApiService(){return getInstance().create(PostPageRetrofitService.class);}
+
+    private static Retrofit getInstance(){
+        Gson gson = new GsonBuilder().setLenient().create();
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
+}
