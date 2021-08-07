49f6d3fcc66e1424a21bde1306b0350c838aa5a1
sein0211 <sein4037@gmail.com>
2021-08-07 16:39:22 +0900

    로그아웃,탈퇴

--- app/src/main/java/com/starrynight/tourapiproject/myPage/myPageRetrofit/MyPageRetrofitService.java
+++ app/src/main/java/com/starrynight/tourapiproject/myPage/myPageRetrofit/MyPageRetrofitService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -27,13 +28,17 @@ public interface MyPageRetrofitService {
     @PUT("user/{userId}/profileImage")
     Call<Void> updateProfileImage(@Path("userId") Long userId, @Body User3 profileImage);
 
-    @PUT("user/{userId}/password/{password}")
-    Call<Void> updatePassword(@Path("userId") Long userId, @Path("password") String password);
+    @PUT("user/{userId}/password/{originPwd}/{newPwd}")
+    Call<Boolean> updatePassword(@Path("userId") Long userId, @Path("originPwd") String originPwd, @Path("newPwd") String newPwd);
 
     @GET("user/{userId}/myHashTag")
     Call<List<String>> getMyHashTag(@Path("userId") Long userId);
 
     @GET("myWishPost/{userId}")
     Call<List<MyPostWish>> getMyWishPost(@Path("userId") Long userId);
+
+    @DELETE("user/{userId}")
+    Call<Void> deleteUser(@Path("userId") Long userId);
+
 
 }
