2f8ebe3b5d3e34f2427d9340bb26db2d542234cc
gyulchyoung <hacha2011@naver.com>
2021-08-08 23:48:30 +0900

    kakao login add signup before hashtag

--- app/src/main/java/com/starrynight/tourapiproject/signUpPage/SessionCallback.java
+++ app/src/main/java/com/starrynight/tourapiproject/signUpPage/SessionCallback.java
@@ -7,10 +7,12 @@ import com.kakao.network.ErrorResult;
 import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.MeV2ResponseCallback;
 import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.Gender;
 import com.kakao.usermgmt.response.model.Profile;
 import com.kakao.usermgmt.response.model.UserAccount;
 import com.kakao.util.OptionalBoolean;
 import com.kakao.util.exception.KakaoException;
+import com.starrynight.tourapiproject.signUpPage.signUpRetrofit.KakaoUserParams;
 
 public class SessionCallback implements ISessionCallback {
 
@@ -28,7 +30,8 @@ public class SessionCallback implements ISessionCallback {
     }
 
     //사용자 정보 요청
-    private void requestMe() {
+    public KakaoUserParams requestMe() {
+        KakaoUserParams kakaoUserParams = new KakaoUserParams();
         UserManagement.getInstance().me(new MeV2ResponseCallback() {
             @Override
             public void onSessionClosed(ErrorResult errorResult) {
@@ -49,16 +52,42 @@ public class SessionCallback implements ISessionCallback {
 
                     // 이메일
                     String email = kakaoAccount.getEmail();
+                    if (email != null) {
+                        Log.d("KAKAO_API", "onSuccess:getEmail " + email);
+                        kakaoUserParams.setEmail(email);
+                    } else {
+                        Log.d("KAKAO_API", "onSuccess:email null ");
+                    }
+                    if (kakaoAccount.getAgeRange() != null) {
+                        Log.d("KAKAO_API", "onSuccess:get ageRange " + kakaoAccount.getAgeRange());
+                        kakaoUserParams.setAgeRange(kakaoAccount.getAgeRange().toString());
+                    }
+                    if (kakaoAccount.getBirthday() != null) {
+                        Log.d("KAKAO_API", "onSuccess:get Birthday " + kakaoAccount.getBirthday());
+                        kakaoUserParams.setBirthDay(kakaoAccount.getBirthday());
+                    }
+                    if (kakaoAccount.getGender()!=null) {
+                        Log.d("KAKAO_API", "onSuccess:get Gender " + kakaoAccount.getGender());
+                        if(kakaoAccount.getGender()== Gender.FEMALE)
+                            kakaoUserParams.setSex(true);
+                        else if (kakaoAccount.getGender()== Gender.MALE)
+                            kakaoUserParams.setSex(false);
+                    }
+
                     Profile profile = kakaoAccount.getProfile();
                     if (profile ==null){
                         Log.d("KAKAO_API", "onSuccess:profile null ");
                     }else{
                         Log.d("KAKAO_API", "onSuccess:getProfileImageUrl "+profile.getProfileImageUrl());
+                        kakaoUserParams.setProfileImage(profile.getProfileImageUrl());
                         Log.d("KAKAO_API", "onSuccess:getNickname "+profile.getNickname());
+                        kakaoUserParams.setNickName(profile.getNickname());
                     }
                     if (email != null) {
 
-                        Log.d("KAKAO_API", "onSuccess:email "+email);
+                        Log.d("KAKAO_API", "onSuccess:email " + email);
+                    } else {
+                        Log.d("KAKAO_API", "onSuccess:email null ");
                     }
 
                     // 프로필
@@ -67,8 +96,11 @@ public class SessionCallback implements ISessionCallback {
                     if (_profile != null) {
 
                         Log.d("KAKAO_API", "nickname: " + _profile.getNickname());
+                        kakaoUserParams.setNickName(profile.getNickname());
                         Log.d("KAKAO_API", "profile image: " + _profile.getProfileImageUrl());
+                        kakaoUserParams.setProfileImage(profile.getProfileImageUrl());
                         Log.d("KAKAO_API", "thumbnail image: " + _profile.getThumbnailImageUrl());
+
 
                     } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
                         // 동의 요청 후 프로필 정보 획득 가능
@@ -82,5 +114,6 @@ public class SessionCallback implements ISessionCallback {
 
             }
         });
+        return kakaoUserParams;
     }
 }
