52ea3d953ddc15ea10e00de6202a4d40c6f7940c
dingding-21 <phj8221@naver.com>
2021-08-10 13:19:36 +0900

    오늘 볼 수 있는 천체 layout

--- app/src/main/java/com/starrynight/tourapiproject/starPage/TonightSkyFragment.java
+++ app/src/main/java/com/starrynight/tourapiproject/starPage/TonightSkyFragment.java
@@ -6,24 +6,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.Adapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.starrynight.tourapiproject.MainActivity;
 import com.starrynight.tourapiproject.R;
+import com.starrynight.tourapiproject.starPage.starItemPage.OnStarItemClickListener;
+import com.starrynight.tourapiproject.starPage.starItemPage.StarItem;
+import com.starrynight.tourapiproject.starPage.starItemPage.StarViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TonightSkyFragment extends Fragment {
-
-    //    private BottomAppBar bottomAppBar;
+    //bottomSheet 관련
     private LinearLayout bottomSheet;
     private BottomSheetBehavior bottomSheetBehavior;
 
     private EditText editText;
 
+    //recyclerview 관련
+    RecyclerView recyclerView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,7 +66,8 @@ public class TonightSkyFragment extends Fragment {
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
 
 
-        ImageButton backBtn =(ImageButton) v.findViewById(R.id.star_bacK_btn);
+        // 뒤로 가기 버튼 클릭 이벤트
+        ImageButton backBtn =(ImageButton) v.findViewById(R.id.star_back_btn);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,6 +76,40 @@ public class TonightSkyFragment extends Fragment {
             }
         });
 
+        // recyclerview 설정
+        recyclerView = v.findViewById(R.id.today_cel_recycler);
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(),3) ;
+        recyclerView.setLayoutManager(layoutManager);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+//        recyclerView.setLayoutManager(layoutManager);
+
+        StarViewAdapter adapter = new StarViewAdapter();
+        recyclerView.setAdapter(adapter);
+
+        adapter.addItem(new StarItem("별자리1", "https://cdn.pixabay.com/photo/2015/02/17/08/25/horoscope-639127_960_720.jpg"));
+        adapter.addItem(new StarItem("별자리", "https://cdn.pixabay.com/photo/2015/02/17/08/25/horoscope-639127_960_720.jpg"));
+        adapter.addItem(new StarItem("별자리1", "https://cdn.pixabay.com/photo/2015/02/17/08/25/horoscope-639127_960_720.jpg"));
+        adapter.addItem(new StarItem("별자리", "https://cdn.pixabay.com/photo/2015/02/17/08/25/horoscope-639127_960_720.jpg"));
+        adapter.addItem(new StarItem("별자리1", "https://cdn.pixabay.com/photo/2015/02/17/08/25/horoscope-639127_960_720.jpg"));
+        adapter.addItem(new StarItem("별자리", "https://cdn.pixabay.com/photo/2015/02/17/08/25/horoscope-639127_960_720.jpg"));
+        adapter.addItem(new StarItem("별자리1", "https://cdn.pixabay.com/photo/2015/02/17/08/25/horoscope-639127_960_720.jpg"));
+        adapter.addItem(new StarItem("별자리", "https://cdn.pixabay.com/photo/2015/02/17/08/25/horoscope-639127_960_720.jpg"));
+
+        recyclerView.setAdapter(adapter);
+
+//        adapter.setOnItemClickListener(new OnStarItemClickListener() {
+//            @Override
+//            public void onItemClick(StarViewAdapter.ViewHolder holder, View view, int position) {
+//                Intent intent = new Intent(getActivity().getApplicationContext(), StarActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
         return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
     }
 }
