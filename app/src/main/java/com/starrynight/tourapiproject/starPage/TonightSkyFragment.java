79fc5c35ae49ec744960fea767901284907cafba
dingding-21 <phj8221@naver.com>
2021-08-11 09:19:17 +0900

    별자리 검색 결과 layout 및 연결

--- app/src/main/java/com/starrynight/tourapiproject/starPage/TonightSkyFragment.java
+++ app/src/main/java/com/starrynight/tourapiproject/starPage/TonightSkyFragment.java
@@ -6,14 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.widget.Adapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
@@ -22,9 +21,6 @@ import com.starrynight.tourapiproject.R;
 import com.starrynight.tourapiproject.starPage.starItemPage.OnStarItemClickListener;
 import com.starrynight.tourapiproject.starPage.starItemPage.StarItem;
 import com.starrynight.tourapiproject.starPage.starItemPage.StarViewAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class TonightSkyFragment extends Fragment {
     //bottomSheet 관련
@@ -54,7 +50,7 @@ public class TonightSkyFragment extends Fragment {
                     @Override
                     public void onGlobalLayout() {
                         editText.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                        bottomSheetBehavior.setPeekHeight(editText.getBottom()+ 50);
+                        bottomSheetBehavior.setPeekHeight(editText.getBottom() + 50);
                     }
                 }
         );
@@ -67,7 +63,7 @@ public class TonightSkyFragment extends Fragment {
 
 
         // 뒤로 가기 버튼 클릭 이벤트
-        ImageButton backBtn =(ImageButton) v.findViewById(R.id.star_back_btn);
+        ImageButton backBtn = (ImageButton) v.findViewById(R.id.star_back_btn);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,8 +74,8 @@ public class TonightSkyFragment extends Fragment {
 
         // recyclerview 설정
         recyclerView = v.findViewById(R.id.today_cel_recycler);
-        GridLayoutManager layoutManager = new GridLayoutManager(getContext(),3) ;
-        recyclerView.setLayoutManager(layoutManager);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 3);
+        recyclerView.setLayoutManager(gridLayoutManager);
 //        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
 //        recyclerView.setLayoutManager(layoutManager);
 
@@ -97,13 +93,23 @@ public class TonightSkyFragment extends Fragment {
 
         recyclerView.setAdapter(adapter);
 
-//        adapter.setOnItemClickListener(new OnStarItemClickListener() {
-//            @Override
-//            public void onItemClick(StarViewAdapter.ViewHolder holder, View view, int position) {
-//                Intent intent = new Intent(getActivity().getApplicationContext(), StarActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        adapter.setOnItemClickListener(new OnStarItemClickListener() {
+            @Override
+            public void onItemClick(StarViewAdapter.ViewHolder holder, View view, int position) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), StarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 모든 천체 보기 버튼 클릭 이벤트
+        TextView celBtn = (TextView) v.findViewById(R.id.all_cel_btn);
+        celBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), StarAllActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return v;
     }
