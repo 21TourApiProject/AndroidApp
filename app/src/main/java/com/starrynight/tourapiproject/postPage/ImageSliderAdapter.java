954c4e781b11212aef6c4ff0a88414f691a516eb
parkjinhyeok348 <wlsgur348@gmail.com>
2021-08-22 21:59:17 +0900

    게시물 이미지, 해시태그 최종 commit

--- app/src/main/java/com/starrynight/tourapiproject/postPage/ImageSliderAdapter.java
+++ app/src/main/java/com/starrynight/tourapiproject/postPage/ImageSliderAdapter.java
@@ -12,13 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.starrynight.tourapiproject.R;
 
-import static android.graphics.BitmapFactory.decodeFile;
+import java.util.ArrayList;
 
 public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.MyViewHolder> {
     private Context context;
-    private String[] sliderImage;
+    private ArrayList<String> sliderImage;
 
-    public ImageSliderAdapter(Context context, String[] sliderImage) {
+    public ImageSliderAdapter(Context context, ArrayList<String> sliderImage) {
         this.context = context;
         this.sliderImage = sliderImage;
     }
@@ -33,13 +33,12 @@ public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.mImageView.setImageBitmap(decodeFile(sliderImage[position]));
-        holder.mImageView.setAdjustViewBounds(true);
+        holder.bindSliderImage(sliderImage.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return sliderImage.length;
+        return sliderImage.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -49,6 +48,12 @@ public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             mImageView = itemView.findViewById(R.id.imageSlider);
+        }
+        public void bindSliderImage(String imageURL) {
+            Glide.with(context)
+                    .load(imageURL)
+                    .into(mImageView);
+
         }
     }
 }
