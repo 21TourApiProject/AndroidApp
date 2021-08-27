d6a0b9a723efa7070bd51255ce01c016f96c97d6
dingding-21 <phj8221@naver.com>
2021-08-27 16:19:53 +0900

    계절별로 별자리 관측법 띄우기

--- app/src/main/java/com/starrynight/tourapiproject/starPage/StarActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/starPage/StarActivity.java
@@ -16,6 +16,10 @@ import com.starrynight.tourapiproject.R;
 import com.starrynight.tourapiproject.starPage.starPageRetrofit.Constellation;
 import com.starrynight.tourapiproject.starPage.starPageRetrofit.RetrofitClient;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -36,11 +40,22 @@ public class StarActivity extends AppCompatActivity {
     Long constId;
     Constellation constData;
 
+    String spring = "0321";
+    String summer = "0622";
+    String fall = "0923";
+    String winter = "1221";
+    String yearEnd = "1231";
+    String yearStart = "0101";
+
+    Integer compareDataSpring, compareDataSummer, compareDataFall, compareDataWinter, compareDataYearEnd, compareDataYearStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star);
+
+
+        //Log.d("compare", String.valueOf(date1.compareTo(date2)));
 
         Intent intent = getIntent();
         constId = (Long) intent.getSerializableExtra("constId");
@@ -71,7 +86,7 @@ public class StarActivity extends AppCompatActivity {
         constObInfoTl.setText("별자리 관측 정보");
 
         constBestMonthTl = constBestMonthTv.findViewById(R.id.title_text);
-        constObInfoTl.setText("가장 보기 좋은 달");
+        constBestMonthTl.setText("가장 보기 좋은 달");
 
         // 별자리 클릭 후 상세 정보 불러오는 api
         Call<Constellation> detailConstCall = RetrofitClient.getApiService().getDetailConst(constId);
@@ -79,11 +94,63 @@ public class StarActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<Constellation> call, Response<Constellation> response) {
                 if (response.isSuccessful()) {
+                    //계절별 별자리 관측법 불러오기
+
+                    Date springDate = null;
+                    Date summerDate = null;
+                    Date fallDate = null;
+                    Date winterDate = null;
+                    Date todayDate = null;
+                    Date yearEndDate = null;
+                    Date yearStartDate = null;
+
+                    SimpleDateFormat sdf = new SimpleDateFormat("MMdd");
+                    try {
+                        Date today = new Date();
+                        springDate = sdf.parse(spring);
+                        summerDate = sdf.parse(summer);
+                        fallDate = sdf.parse(fall);
+                        winterDate = sdf.parse(winter);
+                        yearEndDate = sdf.parse(yearEnd);
+                        yearStartDate = sdf.parse(yearStart);
+                        todayDate = sdf.parse(sdf.format(today));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
                     constData = response.body();
                     Glide.with(StarActivity.this).load(constData.getConstImage()).fitCenter().into(constImage);
                     constName.setText(constData.getConstName());
                     constStory.setText(constData.getConstStory());
-                    constObInfo.setText(constData.getSpringConstMtd());
+
+                    compareDataSpring = todayDate.compareTo(springDate);            //3월 21일
+                    compareDataSummer = todayDate.compareTo(summerDate);            //6월 22일
+                    compareDataFall = todayDate.compareTo(fallDate);                //9월 23일
+                    compareDataWinter = todayDate.compareTo(winterDate);            //12월 21일
+                    compareDataYearStart = todayDate.compareTo(yearStartDate);      //1월 1일
+                    compareDataYearEnd = todayDate.compareTo(yearEndDate);          //12월 31일
+
+                    // 봄(3/21 ~ 6/21)
+                    if ((compareDataSpring == 1 || compareDataSpring == 0) && compareDataSummer == -1) {
+                        constObInfo.setText(constData.getSpringConstMtd());
+                    }
+                    // 여름(6/22 ~ 9/22)
+                    else if ((compareDataSummer == 1 || compareDataSummer == 0) && compareDataFall == -1) {
+                        constObInfo.setText(constData.getSummerConstMtd());
+                    }
+                    // 가을(9/23 ~ 12/20)
+                    else if ((compareDataFall == 1 || compareDataFall == 0) && compareDataWinter == -1) {
+                        constObInfo.setText(constData.getFallConstMtd());
+                    }
+                    // 겨울(12/21 ~ 12/31)
+                    else if ((compareDataWinter == 1 || compareDataWinter == 0) && compareDataYearEnd == -1) {
+                        constObInfo.setText(constData.getWinterConstMtd());
+                    }
+                    // 겨울(01/01 ~ 03/20)
+                    else if ((compareDataYearStart == 1 || compareDataYearStart == 0) && compareDataSpring == -1) {
+                        constObInfo.setText(constData.getWinterConstMtd());
+                    }
+
                     constBestMonth.setText(constData.getConstBestMonth());
 
                     if (constData.getConstPersonality() == null) {
@@ -93,7 +160,7 @@ public class StarActivity extends AppCompatActivity {
                     constPersonalityName.setText(constData.getConstName());
                     constPersonalityPeriod.setText(constData.getConstPeriod());
 
-                    if(constData.getConstFeature() == null){
+                    if (constData.getConstFeature() == null) {
                         constFeature.setVisibility(View.GONE);
                     }
                     Glide.with(StarActivity.this).load(constData.getConstFeature()).fitCenter().into(constFeature);
