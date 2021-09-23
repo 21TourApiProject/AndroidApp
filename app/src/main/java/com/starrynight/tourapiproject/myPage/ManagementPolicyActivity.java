b72b57ffcd5a430fcd712a1e61ec95abc37c4129
sein0211 <sein4037@gmail.com>
2021-09-23 14:57:06 +0900

    설정 페이지

--- app/src/main/java/com/starrynight/tourapiproject/myPage/ManagementPolicyActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/myPage/ManagementPolicyActivity.java
@@ -1,23 +1,23 @@
 package com.starrynight.tourapiproject.myPage;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.starrynight.tourapiproject.R;
 
-public class PersonalDataActivity extends AppCompatActivity {
+public class ManagementPolicyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_data);
+        setContentView(R.layout.activity_management_policy);
 
         //뒤로 가기
-        Button personalDataBack = findViewById(R.id.personalDataBack);
-        personalDataBack.setOnClickListener(new View.OnClickListener() {
+        Button managementPolicyBack = findViewById(R.id.managementPolicyBack);
+        managementPolicyBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
