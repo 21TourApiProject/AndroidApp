ab61091b097543df7688335ea896925970d28575
sein0211 <sein4037@gmail.com>
2021-09-27 00:00:46 +0900

    수정

--- app/src/main/java/com/starrynight/tourapiproject/myPage/ChangeProfileActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/myPage/ChangeProfileActivity.java
@@ -253,26 +253,6 @@ public class ChangeProfileActivity extends AppCompatActivity {
         }
     }
 
-    // 비트맵 둥글게 자르는 함수
-    public Bitmap getCroppedBitmap(Bitmap bitmap) {
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final int color = 0xff424242;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-
-        paint.setAntiAlias(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(color);
-        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
-                bitmap.getWidth() / 2, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, rect, rect, paint);
-        return output;
-    }
-
     // 이미지 uri 경로 함수
     public String getRealPathFromURI(Uri contentUri) {
         String [] proj={MediaStore.Images.Media.DATA};
