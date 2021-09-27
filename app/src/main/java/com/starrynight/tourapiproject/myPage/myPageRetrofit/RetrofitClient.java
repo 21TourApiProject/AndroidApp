85ddc5184ae1b4a03ec0898fe36d51ccaadc4e6b
gyulchyoung <hacha2011@naver.com>
2021-09-27 19:02:02 +0900

    retrofit change

--- app/src/main/java/com/starrynight/tourapiproject/myPage/myPageRetrofit/RetrofitClient.java
+++ app/src/main/java/com/starrynight/tourapiproject/myPage/myPageRetrofit/RetrofitClient.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
 
-    private static final String BASE_URL = "http://192.168.1.66:8080/v1/";
+    private static final String BASE_URL = "http://192.168.35.75:8080/v1/";
 
 
     public static MyPageRetrofitService getApiService(){return getInstance().create(MyPageRetrofitService.class);}
