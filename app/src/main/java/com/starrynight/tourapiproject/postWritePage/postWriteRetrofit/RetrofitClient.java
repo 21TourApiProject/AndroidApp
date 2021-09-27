24043e04c68a2d4158ec81c3c195fba6a4d64600
sein0211 <sein4037@gmail.com>
2021-09-27 19:31:07 +0900

    수정

--- app/src/main/java/com/starrynight/tourapiproject/postWritePage/postWriteRetrofit/RetrofitClient.java
+++ app/src/main/java/com/starrynight/tourapiproject/postWritePage/postWriteRetrofit/RetrofitClient.java
@@ -10,7 +10,7 @@ public class RetrofitClient {
 
 
 
-    private static final String BASE_URL = "http://192.168.1.181:8080/v1/";
+    private static final String BASE_URL = "http://172.30.1.55:8080/v1/";
 
 
 
