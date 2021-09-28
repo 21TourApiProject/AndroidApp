647d5985b0d2e2276dd488d238bdd1ea62d4edf9
parkjinhyeok348 <wlsgur348@gmail.com>
2021-09-28 21:43:02 +0900

    월요일 회의 피드백 commit

--- app/src/main/java/com/starrynight/tourapiproject/postItemPage/PostHashTagItem.java
+++ app/src/main/java/com/starrynight/tourapiproject/postItemPage/PostHashTagItem.java
@@ -1,17 +1,33 @@
 package com.starrynight.tourapiproject.postItemPage;
 
+import android.widget.ImageView;
+
 public class PostHashTagItem {
     String hashTagname;
 
+    ImageView hashTagPin;
+
+    Long ObservationId;
+
     public String getHashTagname() {
         return hashTagname;
+    }
+
+    public ImageView getHashTagPin() {
+        return hashTagPin;
+    }
+
+    public Long getObservationId() {
+        return ObservationId;
     }
 
     public void setHashTagname(String hashTagname) {
         this.hashTagname = hashTagname;
     }
 
-    public PostHashTagItem(String hashTagname) {
+    public PostHashTagItem(String hashTagname, ImageView hashTagPin, Long observationId) {
         this.hashTagname = hashTagname;
+        this.hashTagPin = hashTagPin;
+        ObservationId = observationId;
     }
 }
