2c8a5a7cfc68aea68dc923baa8d4b7d96af6f3f3
gyulchyoung <hacha2011@naver.com>
2021-09-29 00:39:26 +0900

    edit map backbtn

--- app/src/main/java/com/starrynight/tourapiproject/observationPage/observationPageRetrofit/RetrofitClient.java
+++ app/src/main/java/com/starrynight/tourapiproject/observationPage/observationPageRetrofit/RetrofitClient.java
@@ -11,7 +11,7 @@ public class RetrofitClient {
 
 
 
-    private static final String BASE_URL = "http://172.30.1.55:8080/v1/";
+    private static final String BASE_URL = "http://192.168.35.75:8080/v1/";
 
 
 
