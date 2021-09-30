8ee07f0c328bdd610f6754f4317ef81c7e417496
sein0211 <sein4037@gmail.com>
2021-09-30 18:05:53 +0900

    둥글게

--- app/src/main/java/com/starrynight/tourapiproject/touristPointPage/touristPointRetrofit/RetrofitClient.java
+++ app/src/main/java/com/starrynight/tourapiproject/touristPointPage/touristPointRetrofit/RetrofitClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://172.30.1.58:8080/v1/";
+    private static final String BASE_URL = "http://192.168.162.57:8080/v1/";
 
     public static TouristPointPageRetrofitService getApiService(){return getInstance().create(TouristPointPageRetrofitService.class);}
 
