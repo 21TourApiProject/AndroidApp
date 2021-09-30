dd879e3db3fa3f141dc6cb95ada83484fbc7b86a
sein0211 <sein4037@gmail.com>
2021-09-30 23:28:07 +0900

    개운

--- app/src/main/java/com/starrynight/tourapiproject/touristPointPage/search/SearchRetrofitFactory.java
+++ app/src/main/java/com/starrynight/tourapiproject/touristPointPage/search/SearchRetrofitFactory.java
@@ -4,7 +4,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SearchRetrofitFactory {
+
     private static String BASE_URL="https://dapi.kakao.com/";
+
     public static SearchOpenApi create(){
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtAppearTimeModel/WtAppearTimeRetrofit.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtAppearTimeModel/WtAppearTimeRetrofit.java
@@ -2,7 +2,6 @@ package com.starrynight.tourapiproject.weatherPage.wtAppearTimeModel;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.starrynight.tourapiproject.weatherPage.wtMetModel.WtMetInterface;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
