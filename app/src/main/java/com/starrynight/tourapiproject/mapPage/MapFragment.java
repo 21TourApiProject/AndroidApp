81648c6786c25b66ca27a095d7354bf770effd83
gyulchyoung <hacha2011@naver.com>
2021-10-02 00:35:40 +0900

    edit map, observation layout

--- app/src/main/java/com/starrynight/tourapiproject/mapPage/MapFragment.java
+++ app/src/main/java/com/starrynight/tourapiproject/mapPage/MapFragment.java
@@ -143,9 +143,7 @@ public class MapFragment extends Fragment {
         @Override
         public View getCalloutBalloon(MapPOIItem poiItem) {
             //말풍선 내용 설정
-//            BalloonObject bobject = (BalloonObject)poiItem.getUserObject();
             ((TextView) mCalloutBalloon.findViewById(R.id.title)).setText(poiItem.getItemName());
-//            ((TextView) mCalloutBalloon.findViewById(R.id.desc)).setText(bobject.getContent());
             return mCalloutBalloon;
         }
 
@@ -417,12 +415,13 @@ public class MapFragment extends Fragment {
                 LoadingAsyncTask task = new LoadingAsyncTask(getActivity(),5500);
                 task.execute();
 
+                selectFilterItem.setVisibility(View.VISIBLE);
                 selectFilterItem.removeAllViews(); //초기화
                 for(int i=0; i<17; i++){
                     if(area.get(i) == 1){
                         TextView textView = new TextView(getContext());
                         textView.setText(" "+ areaName[i] + " ");
-                        textView.setTextColor(ContextCompat.getColor(getContext(), R.color.purple_200));
+                        textView.setTextColor(ContextCompat.getColor(getContext(), R.color.point_purple));
                         textView.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.hashtags_empty));
                         selectFilterItem.addView(textView);
                     }
@@ -431,7 +430,7 @@ public class MapFragment extends Fragment {
                     if(hashTag.get(i) == 1){
                         TextView textView = new TextView(getContext());
                         textView.setText("#" + hashTagName[i]);
-                        textView.setTextColor(ContextCompat.getColor(getContext(), R.color.purple_200));
+                        textView.setTextColor(ContextCompat.getColor(getContext(), R.color.point_purple));
                         textView.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.hashtags_empty));
                         selectFilterItem.addView(textView);
                     }
@@ -517,12 +516,13 @@ public class MapFragment extends Fragment {
                 LoadingAsyncTask task = new LoadingAsyncTask(getActivity(),5500);
                 task.execute();
 
+                selectFilterItem.setVisibility(View.VISIBLE);
                 selectFilterItem.removeAllViews(); //초기화
                 for(int i=0; i<17; i++){
                     if(area.get(i) == 1){
                         TextView textView = new TextView(getContext());
                         textView.setText(" "+ areaName[i] + " ");
-                        textView.setTextColor(ContextCompat.getColor(getContext(), R.color.purple_200));
+                        textView.setTextColor(ContextCompat.getColor(getContext(), R.color.point_purple));
                         textView.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.hashtags_empty));
                         selectFilterItem.addView(textView);
                     }
@@ -531,7 +531,7 @@ public class MapFragment extends Fragment {
                     if(hashTag.get(i) == 1){
                         TextView textView = new TextView(getContext());
                         textView.setText("#" + hashTagName[i]);
-                        textView.setTextColor(ContextCompat.getColor(getContext(), R.color.purple_200));
+                        textView.setTextColor(ContextCompat.getColor(getContext(), R.color.point_purple));
                         textView.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.hashtags_empty));
                         selectFilterItem.addView(textView);
                     }
@@ -690,7 +690,7 @@ public class MapFragment extends Fragment {
         });
 
         //필터버튼 설정
-        Button filter_btn = (Button) view.findViewById(R.id.map_filterBtn);
+        ImageButton filter_btn = (ImageButton) view.findViewById(R.id.map_filterBtn);
         filter_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -726,7 +726,7 @@ public class MapFragment extends Fragment {
         });
 
         //목록이동
-        Button list_btn = (Button) view.findViewById(R.id.searchList_Btn);
+        ImageButton list_btn = (ImageButton) view.findViewById(R.id.searchList_Btn);
         list_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
