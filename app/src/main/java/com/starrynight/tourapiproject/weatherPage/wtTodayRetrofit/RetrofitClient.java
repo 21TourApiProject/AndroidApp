1ff4000ec77b91c8ab0ac6ea1bd049fa667d02c4
gyulchyoung <hacha2011@naver.com>
2021-10-03 02:55:01 +0900

    edit kakao login

--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtTodayRetrofit/RetrofitClient.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtTodayRetrofit/RetrofitClient.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://192.168.0.35:8080/v1/";
+    private static final String BASE_URL = "http://192.168.35.75:8080/v1/";
 
     public static WtTodayRetrofitService getApiService() {
         return getInstance().create(WtTodayRetrofitService.class);
