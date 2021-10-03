1e419ae6cbaa00e826b9e6e13f5d92331d770156
sein0211 <sein4037@gmail.com>
2021-10-03 13:34:45 +0900

    별자리 도움말 수정

--- app/src/main/java/com/starrynight/tourapiproject/starPage/TonightSkyFragment.java
+++ app/src/main/java/com/starrynight/tourapiproject/starPage/TonightSkyFragment.java
@@ -18,7 +18,6 @@ import android.view.animation.RotateAnimation;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
