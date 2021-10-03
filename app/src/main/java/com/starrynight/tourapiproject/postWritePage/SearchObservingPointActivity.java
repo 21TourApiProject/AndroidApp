1433b0a1e07651a14dd3c3f6cbe90050cbf460ac
gyulchyoung <hacha2011@naver.com>
2021-10-03 12:23:35 +0900

    delete useless

--- app/src/main/java/com/starrynight/tourapiproject/postWritePage/SearchObservingPointActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/postWritePage/SearchObservingPointActivity.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Filterable;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
