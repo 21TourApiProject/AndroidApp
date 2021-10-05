e47c4ac07bcaefa85323b99a19ecf8435f445cea
sein0211 <sein4037@gmail.com>
2021-10-05 16:30:11 +0900

    전체 동의, 전화번호인증수정

--- app/src/main/java/com/starrynight/tourapiproject/signUpPage/SelectMyHashTagActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/signUpPage/SelectMyHashTagActivity.java
@@ -20,6 +20,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import retrofit2.Call;
@@ -30,7 +31,14 @@ public class SelectMyHashTagActivity extends AppCompatActivity {
     List<MyHashTagParams> myHashTagParams = new ArrayList<>();
     String email;
     Long userId;
+    ArrayList<String> hashtag;
     String[] clicked = new String[22];
+
+    HashMap<String, Integer> hashTagMap = new HashMap<String, Integer>();
+    String[] hashTagName = {"공기 좋은", "깔끔한", "감성적인", "이색적인", "인생샷", "전문적인", "캠핑", "차박", "뚜벅이", "드라이브",
+            "반려동물", "한적한", "근교", "도심 속", "연인", "가족", "친구", "혼자", "가성비", "소확행", "럭셔리한", "경치 좋은"};
+
+    Button[] buttons = new Button[22];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +48,88 @@ public class SelectMyHashTagActivity extends AppCompatActivity {
         Intent intent = getIntent();
         email = (String) intent.getSerializableExtra("email"); //전 페이지에서 받아온 사용자 이메일
         userId = (Long) intent.getSerializableExtra("userId"); //전 페이지에서 받아온 사용자 id
+        hashtag = (ArrayList<String>) intent.getSerializableExtra("hashtag"); //전 페이지에서 받아온 사용자 해시태그
 
         for(int i=0; i<22; i++){
             clicked[i]="";
         }
+
+        if(hashtag != null){
+            int i = 0;
+            for (String name : hashTagName){
+                hashTagMap.put(name, i);
+                i++;
+            }
+
+            for (String h : hashtag){
+                System.out.println("h = " + h);
+                System.out.println("hashTagMap.get(h) = " + hashTagMap.get(h));
+                clicked[hashTagMap.get(h)]=h;
+            }
+        }
+
+        Button ht1 = findViewById(R.id.ht1);
+        Button ht2 = findViewById(R.id.ht2);
+        Button ht3 = findViewById(R.id.ht3);
+        Button ht4 = findViewById(R.id.ht4);
+        Button ht5 = findViewById(R.id.ht5);
+        Button ht6 = findViewById(R.id.ht6);
+        Button ht7 = findViewById(R.id.ht7);
+        Button ht8 = findViewById(R.id.ht8);
+        Button ht9 = findViewById(R.id.ht9);
+        Button ht10 = findViewById(R.id.ht10);
+        Button ht11 = findViewById(R.id.ht11);
+        Button ht12 = findViewById(R.id.ht12);
+        Button ht13 = findViewById(R.id.ht13);
+        Button ht14 = findViewById(R.id.ht14);
+        Button ht15 = findViewById(R.id.ht15);
+        Button ht16 = findViewById(R.id.ht16);
+        Button ht17 = findViewById(R.id.ht17);
+        Button ht18 = findViewById(R.id.ht18);
+        Button ht19 = findViewById(R.id.ht19);
+        Button ht20 = findViewById(R.id.ht20);
+        Button ht21 = findViewById(R.id.ht21);
+        Button ht22 = findViewById(R.id.ht22);
+        buttons[0] = ht1;
+        buttons[1] = ht2;
+        buttons[2] = ht3;
+        buttons[3] = ht4;
+        buttons[4] = ht5;
+        buttons[5] = ht6;
+        buttons[6] = ht7;
+        buttons[7] = ht8;
+        buttons[8] = ht9;
+        buttons[9] = ht10;
+        buttons[10] = ht11;
+        buttons[11] = ht12;
+        buttons[12] = ht13;
+        buttons[13] = ht14;
+        buttons[14] = ht15;
+        buttons[15] = ht16;
+        buttons[16] = ht17;
+        buttons[17] = ht18;
+        buttons[18] = ht19;
+        buttons[19] = ht20;
+        buttons[20] = ht21;
+        buttons[21] = ht22;
+
+        for(int i=0; i<22; i++){
+            if (!clicked[i].equals("")){
+                buttons[i].setTag("isClicked");
+                buttons[i].setBackground(ContextCompat.getDrawable(this, R.drawable.selectmyhashtag_hashtag));
+            }
+        }
+
+        Button selectHashTagBack = findViewById(R.id.selectHashTagBack); //뒤로가기
+        if (email == null){
+            selectHashTagBack.setVisibility(View.VISIBLE);
+        }
+        selectHashTagBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         Button finSelectHt = findViewById(R.id.finSelectHt);
         finSelectHt.setOnClickListener(new View.OnClickListener() {
@@ -144,6 +230,10 @@ public class SelectMyHashTagActivity extends AppCompatActivity {
         }
     }
 
-    @Override public void onBackPressed() {}
+    @Override public void onBackPressed() {
+        if (email == null){
+            finish();
+        }
+    }
 
 }
