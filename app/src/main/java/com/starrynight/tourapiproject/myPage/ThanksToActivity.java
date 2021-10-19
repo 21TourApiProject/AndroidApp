5c1aaedca68d1dc00ad265cbf5e9755ebef06ac0
sein0211 <sein4037@gmail.com>
2021-10-20 01:53:06 +0900

    기여한사람

--- app/src/main/java/com/starrynight/tourapiproject/myPage/ThanksToActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/myPage/ThanksToActivity.java
@@ -3,6 +3,8 @@ package com.starrynight.tourapiproject.myPage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.starrynight.tourapiproject.R;
 
@@ -12,5 +14,13 @@ public class ThanksToActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thanks_to);
+
+        FrameLayout thanksToBack = findViewById(R.id.thanksToBack);
+        thanksToBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
