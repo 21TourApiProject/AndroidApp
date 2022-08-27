00474d88dc3cff18f62841b1b05ae6a62910e316
gyulchyoung <hacha2011@naver.com>
2022-08-28 02:52:46 +0900

    [Refactor] 서버 주소값 분리

---
+++ app/src/main/java/com/starrynight/tourapiproject/retrofitConfig/TaskServer.java
@@ -0,0 +1,15 @@
+package com.starrynight.tourapiproject.retrofitConfig;
+
+import com.starrynight.tourapiproject.BuildConfig;
+
+public class TaskServer {
+
+    public static final String v1Server = BuildConfig.SPRING_SERVER_V1;
+//    public static final String v1Server = "http://localhost:8080/v1/";
+
+    public static final String kkoMapURL = "https://dapi.kakao.com/";
+
+    public static final String fineDustURL = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/";
+
+    public static final String openWeatherURL = "https://api.openweathermap.org/data/2.5/";
+}
