69f2fd61273c26bedf20eda5ad99c3338b27ccd4
dingding-21 <phj8221@naver.com>
2022-09-03 07:13:23 +0900

    [Comment] 오늘의 날씨 주석 추가

--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtTodayRetrofit/WtTodayRetrofitService.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtTodayRetrofit/WtTodayRetrofitService.java
@@ -4,6 +4,18 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
+/**
+ * @className : RetrofitClient
+ * @description : 오늘의 날씨 관련 Retrofit 주소 설정입니다.
+ * @modification : 2022-09-03 (hyeonz) 주석추가
+ * @author : hyeonz
+ * @date : 2022-09-03
+ * @version : 1.0
+====개정이력(Modification Information)====
+수정일        수정자        수정내용
+-----------------------------------------
+hyeonz       2022-09-03   주석추가
+ */
 public interface WtTodayRetrofitService {
     @GET("wtToday/{todayWtId}")
     Call<WtTodayParams> getTodayWeatherInfo(@Path("todayWtId") int todayWtId);
