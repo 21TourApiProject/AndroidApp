ae8916fd9b9c19abfb3d2e4952fed33597fddf58
dingding-21 <phj8221@naver.com>
2022-09-03 06:47:47 +0900

    [Comment] 날씨-지역 주석 추가

--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtAreaRetrofit/WtArea.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtAreaRetrofit/WtArea.java
@@ -2,6 +2,18 @@ package com.starrynight.tourapiproject.weatherPage.wtAreaRetrofit;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * @className : WtArea
+ * @description : 날씨-지역 DTO 입니다.
+ * @modification : 2022-09-03 (hyeonz) 주석추가
+ * @author : hyeonz
+ * @date : 2022-09-03
+ * @version : 1.0
+====개정이력(Modification Information)====
+수정일        수정자        수정내용
+-----------------------------------------
+hyeonz       2022-09-03   주석추가
+ */
 public class WtArea {
     @SerializedName("wtAreaId")
     private Long wtAreaId;
