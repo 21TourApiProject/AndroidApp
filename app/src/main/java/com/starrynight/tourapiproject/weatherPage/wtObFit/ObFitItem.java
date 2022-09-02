6a95cfb0029dc1073962c79602da953820e21ed9
dingding-21 <phj8221@naver.com>
2022-09-03 07:09:28 +0900

    [Comment] 날씨-별 관측적합도 주석 추가

--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtObFit/ObFitItem.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtObFit/ObFitItem.java
@@ -1,5 +1,17 @@
 package com.starrynight.tourapiproject.weatherPage.wtObFit;
 
+/**
+ * @className : ObFitItem
+ * @description : 날씨페이지 별 관측적합도 Item입니다.
+ * @modification : 2022-09-03 (hyeonz) 주석추가
+ * @author : hyeonz
+ * @date : 2022-09-03
+ * @version : 1.0
+====개정이력(Modification Information)====
+수정일        수정자        수정내용
+-----------------------------------------
+hyeonz       2022-09-03   주석추가
+ */
 public class ObFitItem {
     private int obFitImage;
     private String obFitTime;
