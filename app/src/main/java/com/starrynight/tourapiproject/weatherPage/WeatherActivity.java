b568480e31f26f700f7a9653d0ff287e75cdc635
dingding-21 <phj8221@naver.com>
2022-09-03 07:22:16 +0900

    [Comment] 날씨페이지 관련 주석 추가

--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/WeatherActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/WeatherActivity.java
@@ -63,6 +63,18 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+/**
+ * @className : WeatherActivity
+ * @description : 날씨 페이지입니다.
+ * @modification : 2022-09-03 (hyeonz) 주석추가
+ * @author : hyeonz
+ * @date : 2022-09-03
+ * @version : 1.0
+====개정이력(Modification Information)====
+수정일        수정자        수정내용
+-----------------------------------------
+hyeonz       2022-09-03   주석추가
+ */
 public class WeatherActivity extends AppCompatActivity {
     WeatherLoadingDialog dialog;
     WeatherActivity.LoadingAsyncTask task;
--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/WeatherLoadingDialog.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/WeatherLoadingDialog.java
@@ -9,6 +9,18 @@ import androidx.annotation.NonNull;
 
 import com.starrynight.tourapiproject.R;
 
+/**
+ * @className : WeatherLoadingDialog
+ * @description : 날씨 페이지 누른 후 나오는 로딩 Dialog입니다.
+ * @modification : 2022-09-03 (hyeonz) 주석추가
+ * @author : hyeonz
+ * @date : 2022-09-03
+ * @version : 1.0
+====개정이력(Modification Information)====
+수정일        수정자        수정내용
+-----------------------------------------
+hyeonz       2022-09-03   주석추가
+ */
 public class WeatherLoadingDialog extends Dialog {
     public WeatherLoadingDialog(@NonNull Context context) {
         super(context);
