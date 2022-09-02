232c3ce8299bbcfc5424ac13b04c3a75a9104baa
dingding-21 <phj8221@naver.com>
2022-09-03 07:04:05 +0900

    [Comment] 날씨 상세정보 주석 추가

--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtMetModel/FeelsLike.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtMetModel/FeelsLike.java
@@ -3,6 +3,18 @@ package com.starrynight.tourapiproject.weatherPage.wtMetModel;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * @className : FeelsLike
+ * @description : 체감 날씨 DTO입니다.
+ * @modification : 2022-09-03 (hyeonz) 주석추가
+ * @author : hyeonz
+ * @date : 2022-09-03
+ * @version : 1.0
+====개정이력(Modification Information)====
+수정일        수정자        수정내용
+-----------------------------------------
+hyeonz       2022-09-03   주석추가
+ */
 public class FeelsLike {
 
     @SerializedName("day")
--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtMetModel/WtMetRetrofit.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtMetModel/WtMetRetrofit.java
@@ -7,6 +7,18 @@ import com.starrynight.tourapiproject.retrofitConfig.TaskServer;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * @className : WtMetRetrofit
+ * @description : 날씨 상세정보 관련 Retrofit 설정입니다.
+ * @modification : 2022-09-03 (hyeonz) 주석추가
+ * @author : hyeonz
+ * @date : 2022-09-03
+ * @version : 1.0
+====개정이력(Modification Information)====
+수정일        수정자        수정내용
+-----------------------------------------
+hyeonz       2022-09-03   주석추가
+ */
 public class WtMetRetrofit {
     private static String BASE_URL = TaskServer.openWeatherURL;
 
