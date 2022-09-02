3e2e0fa4922a080ba3e3b1c3a63e62ad9149a3c0
dingding-21 <phj8221@naver.com>
2022-09-03 06:49:04 +0900

    [Comment] 날씨-미세먼지 주석 추가

--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtFineDustModel/Body.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtFineDustModel/Body.java
@@ -4,6 +4,19 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+
+/**
+ * @className : Body
+ * @description : 날씨-미세먼지 Body DTO 입니다.
+ * @modification : 2022-09-03 (hyeonz) 주석추가
+ * @author : hyeonz
+ * @date : 2022-09-03
+ * @version : 1.0
+====개정이력(Modification Information)====
+수정일        수정자        수정내용
+-----------------------------------------
+hyeonz       2022-09-03   주석추가
+ */
 public class Body {
     @SerializedName("items")
     private List<Item> items;
--- app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtFineDustModel/Response.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage/wtFineDustModel/Response.java
@@ -2,6 +2,18 @@ package com.starrynight.tourapiproject.weatherPage.wtFineDustModel;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * @className : Response
+ * @description : 날씨-미세먼지 Response DTO입니다.
+ * @modification : 2022-09-03 (hyeonz) 주석추가
+ * @author : hyeonz
+ * @date : 2022-09-03
+ * @version : 1.0
+====개정이력(Modification Information)====
+수정일        수정자        수정내용
+-----------------------------------------
+hyeonz       2022-09-03   주석추가
+ */
 public class Response {
     @SerializedName("body")
     private Body body;
